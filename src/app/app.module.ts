5eca9a997a22058261a633869632dec9a3500245
Ilyoskhuja <mr.ikromhodjayev@gmail.com>
2021-11-11 17:16:32 +0500

    signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,11 @@ import {RouterModule} from "@angular/router";
 import {routesConfig} from "./routes.config";
 import {LessonsService} from "./services/lessons.service";
 import {ReactiveFormsModule} from "@angular/forms";
-
-
+import { AuthService } from './services/auth.service';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/shareReplay';
+import 'rxjs/add/operator/do';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,7 @@ import {ReactiveFormsModule} from "@angular/forms";
       RouterModule.forRoot(routesConfig),
       ReactiveFormsModule
   ],
-  providers: [LessonsService],
+  providers: [LessonsService,AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
